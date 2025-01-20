e4665ebd5f8c0af66af05eb01ee9f55ead0de2c9
Daniil Veres <daniaveres@gmail.com>
2025-01-20 21:12:11 +0200

    [JsonIgnore] attribute added to UserId in EditPrinterRequest to avoid passing it via HTTP

--- PrintMe.Server/Models/Api/ApiRequest/EditPrinterRequest.cs
+++ PrintMe.Server/Models/Api/ApiRequest/EditPrinterRequest.cs
@@ -1,9 +1,11 @@
+using Newtonsoft.Json;
 using PrintMe.Server.Models.DTOs.PrinterDto;
 
 namespace PrintMe.Server.Models.Api.ApiRequest;
 
 public class EditPrinterRequest
 {
+    [JsonIgnore]
     public int UserId { get; set; }
     public int PrinterID { get; set; }
     public string Description { get; set; }
