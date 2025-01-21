595a1dc34d100e55b39beee3d016284c5eec3969
Daniil Veres <daniaveres@gmail.com>
2025-01-21 19:29:29 +0200

    Filter hotfix

--- PrintMe.Server/Persistence/Repository/RequestRepository.cs
+++ PrintMe.Server/Persistence/Repository/RequestRepository.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using Microsoft.EntityFrameworkCore;
 using PrintMe.Server.Models.Filters;
 using PrintMe.Server.Persistence.Entities;
@@ -7,152 +6,165 @@ namespace PrintMe.Server.Persistence.Repository;
 
 public class RequestRepository(PrintMeDbContext context)
 {
-    public async Task<IEnumerable<Request>> GetAllRequestsAsync(RequestFilter filter = null)
-    {
-        var statusId = filter.StatusId ?? 0;
-        var typeId = filter.TypeId ?? 0;
-        if (statusId != 0 && typeId != 0)
-        {
-            return await context
-                .Requests
-                .AsQueryable()
-                .Where(request => request.RequestStatusId == statusId && request.RequestTypeId == typeId)
-                .ToListAsync();
-        } 
-        else if (statusId == 0 && typeId != 0)
-        {
-            return await context
-                .Requests
-                .AsQueryable()
-                .Where(request => request.RequestTypeId == typeId)
-                .ToListAsync();
-        } 
-        else if (typeId == 0 && statusId != 0)
-        {
-            return await context
-                .Requests
-                .AsQueryable()
-                .Where(request => request.RequestStatusId == statusId)
-                .ToListAsync();
-        }
-        else
-        {
-            return await context
-                .Requests
-                .AsQueryable()
-                .ToListAsync();
-        }
-        
-    }
+	public async Task<IEnumerable<Request>> GetAllRequestsAsync(RequestFilter filter = null)
+	{
+		if (filter is null || filter.StatusId == 0 && filter.TypeId == 0)
+		{
+			return await context
+				.Requests
+				.AsQueryable()
+				.ToListAsync();
+		}
 
-    public async Task<Request> GetRequestByIdAsync(int requestId)
-    {
-        return await context
-            .Requests
-            .AsQueryable()
-            .Include(request => request.PrintMaterials)
-            .FirstOrDefaultAsync(request => request.RequestId == requestId);
-    }
+		var statusId = filter.StatusId ?? 0;
+		var typeId = filter.TypeId ?? 0;
+		if (statusId != 0 && typeId != 0)
+		{
+			return await context
+				.Requests
+				.AsQueryable()
+				.Where(request => request.RequestStatusId == statusId && request.RequestTypeId == typeId)
+				.ToListAsync();
+		}
 
-    public async Task<IEnumerable<Request>> GetRequestsByUserIdAsync(int userId, RequestFilter filter = null)
-    {
-        var statusId = filter.StatusId ?? 0;
-        var typeId = filter.TypeId ?? 0;
-        if (statusId != 0 && typeId != 0)
-        {
-            return await context
-                .Requests
-                .AsQueryable()
-                .Where(request => request.UserSenderId == userId && request.RequestStatusId == statusId && request.RequestTypeId == typeId)
-                .ToListAsync();
-        } 
-        else if (statusId == 0 && typeId != 0)
-        {
-            return await context
-                .Requests
-                .AsQueryable()
-                .Where(request => request.UserSenderId == userId && request.RequestTypeId == typeId)
-                .ToListAsync();
-        } 
-        else if (typeId == 0 && statusId != 0)
-        {
-            return await context
-                .Requests
-                .AsQueryable()
-                .Where(request => request.UserSenderId == userId && request.RequestStatusId == statusId)
-                .ToListAsync();
-        }
-        else
-        {
-            return await context
-                .Requests
-                .AsQueryable()
-                .Where(request => request.UserSenderId == userId)
-                .ToListAsync();
-        }
-    }
-    
-    public async Task<int> GetRequestStatusReasonIdByNameAsync(string reason)
-    {
-        var requestStatusReason = await context
-            .RequestStatusReasons
-            .AsQueryable()
-            .FirstOrDefaultAsync(requestStatusReason => requestStatusReason.Reason == reason);
+		if (statusId == 0 && typeId != 0)
+		{
+			return await context
+				.Requests
+				.AsQueryable()
+				.Where(request => request.RequestTypeId == typeId)
+				.ToListAsync();
+		}
 
-        return requestStatusReason.RequestStatusReasonId;
-    }
+		if (typeId == 0 && statusId != 0)
+		{
+			return await context
+				.Requests
+				.AsQueryable()
+				.Where(request => request.RequestStatusId == statusId)
+				.ToListAsync();
+		}
 
-    public async Task UpdateRequestAsync(Request request)
-    {
-        var existingEntity = await context.Requests.FindAsync(request.RequestId);
-        if (existingEntity != null)
-        {
-            context.Entry(existingEntity).State = EntityState.Detached;
-        }
+		return await context
+			.Requests
+			.AsQueryable()
+			.ToListAsync();
+	}
 
-        context.Requests.Update(request);
-        await context.SaveChangesAsync();
-    }
+	public async Task<Request> GetRequestByIdAsync(int requestId)
+	{
+		return await context
+			.Requests
+			.AsQueryable()
+			.Include(request => request.PrintMaterials)
+			.FirstOrDefaultAsync(request => request.RequestId == requestId);
+	}
 
-    public async Task AddPrinterRequestAsync(Request request)
-    {
-        await context.Requests.AddAsync(request);
-        await context.SaveChangesAsync();
-    }
+	public async Task<IEnumerable<Request>> GetRequestsByUserIdAsync(int userId, RequestFilter filter = null)
+	{
+		if (filter is null || filter.StatusId == 0 && filter.TypeId == 0)
+		{
+			return await context
+				.Requests
+				.AsQueryable()
+				.Where(request => request.UserSenderId == userId)
+				.ToListAsync();
+		}
 
-    public async Task EditPrinterRequestAsync(Request request)
-    {
-        context.Requests.Update(request);
-        await context.SaveChangesAsync();
-    }
+		var statusId = filter.StatusId ?? 0;
+		var typeId = filter.TypeId ?? 0;
+		if (statusId != 0 && typeId != 0)
+		{
+			return await context
+				.Requests
+				.AsQueryable()
+				.Where(request => request.UserSenderId == userId && request.RequestStatusId == statusId &&
+				                  request.RequestTypeId == typeId)
+				.ToListAsync();
+		}
 
-    public async Task<string> GetRequestTypeNameByIdAsync(int requestTypeId)
-    {
-        var requestType = await context
-            .RequestTypes
-            .AsQueryable()
-            .FirstOrDefaultAsync(requestType => requestType.RequestTypeId == requestTypeId);
+		if (statusId == 0 && typeId != 0)
+		{
+			return await context
+				.Requests
+				.AsQueryable()
+				.Where(request => request.UserSenderId == userId && request.RequestTypeId == typeId)
+				.ToListAsync();
+		}
 
-        return requestType.Type;
-    }
+		if (typeId == 0 && statusId != 0)
+		{
+			return await context
+				.Requests
+				.AsQueryable()
+				.Where(request => request.UserSenderId == userId && request.RequestStatusId == statusId)
+				.ToListAsync();
+		}
 
-    public async Task AddPrinterRequestMaterialsAsync(int requestId, IEnumerable<int> materialIds)
-    {
-        var request = await context.Requests.FindAsync(requestId);
-        if (request == null)
-        {
-            return;
-        }
+		throw new Exception("Invalid filter");
+	}
 
-        foreach (var materialId in materialIds)
-        {
-            var material = await context.PrintMaterials1.FindAsync(materialId);
-            if (material != null)
-            {
-                request.PrintMaterials.Add(material);
-            }
-        }
-        
-        await context.SaveChangesAsync();
-    }
+	public async Task<int> GetRequestStatusReasonIdByNameAsync(string reason)
+	{
+		var requestStatusReason = await context
+			.RequestStatusReasons
+			.AsQueryable()
+			.FirstOrDefaultAsync(requestStatusReason => requestStatusReason.Reason == reason);
+
+		return requestStatusReason.RequestStatusReasonId;
+	}
+
+	public async Task UpdateRequestAsync(Request request)
+	{
+		var existingEntity = await context.Requests.FindAsync(request.RequestId);
+		if (existingEntity != null)
+		{
+			context.Entry(existingEntity).State = EntityState.Detached;
+		}
+
+		context.Requests.Update(request);
+		await context.SaveChangesAsync();
+	}
+
+	public async Task AddPrinterRequestAsync(Request request)
+	{
+		await context.Requests.AddAsync(request);
+		await context.SaveChangesAsync();
+	}
+
+	public async Task EditPrinterRequestAsync(Request request)
+	{
+		context.Requests.Update(request);
+		await context.SaveChangesAsync();
+	}
+
+	public async Task<string> GetRequestTypeNameByIdAsync(int requestTypeId)
+	{
+		var requestType = await context
+			.RequestTypes
+			.AsQueryable()
+			.FirstOrDefaultAsync(requestType => requestType.RequestTypeId == requestTypeId);
+
+		return requestType.Type;
+	}
+
+	public async Task AddPrinterRequestMaterialsAsync(int requestId, IEnumerable<int> materialIds)
+	{
+		var request = await context.Requests.FindAsync(requestId);
+		if (request == null)
+		{
+			return;
+		}
+
+		foreach (var materialId in materialIds)
+		{
+			var material = await context.PrintMaterials1.FindAsync(materialId);
+			if (material != null)
+			{
+				request.PrintMaterials.Add(material);
+			}
+		}
+
+		await context.SaveChangesAsync();
+	}
 }
